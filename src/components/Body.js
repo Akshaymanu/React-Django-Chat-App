9ec04cd8d70785c908f7300faf337ad8714648e4
Akshaymanu <akshaymanu57@gmail.com>
2024-06-28 08:52:51 +0530

    checking css working

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,15 @@
 const Body = () => {
   return (
     <div>
+
+      <main>
+      <div class="grid-container">
       <h3>Body</h3>
+        <div class="item2">Chats</div>
+        <div class="item3">Main</div>  
+        <div class="item4">Profile</div>
+      </div>
+      </main>
     </div>
   )
 }
